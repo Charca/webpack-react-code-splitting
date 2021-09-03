0359a604125db8283f61aa2717a10c65e916c1d3
Maxi Ferreira <charca@gmail.com>
2021-09-03 00:44:31 -0700

    Added bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,58 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 const apps = ["dashboard", "settings", "teams", "users"];
 
-module.exports = {
-  context: path.resolve(__dirname, "src", "apps"),
-  entry: apps.reduce((acc, app) => {
-    acc[app] = `./${app}/index.js`;
-    return acc;
-  }, {}),
-  output: {
-    filename: `[name].[contenthash].js`,
-    chunkFilename: `[name].chunk.[contenthash].js`,
-    path: path.join(__dirname, "dist"),
-    clean: true,
-  },
-  devServer: {
-    port: 3000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ["", ".js", ".jsx"],
-  },
-  plugins: apps.map(
-    (app) =>
-      new HtmlWebpackPlugin({
-        chunks: [app],
-        filename: path.join(__dirname, "dist", app, "index.html"),
-        template: path.join(__dirname, "src", "index.html"),
-      })
-  ),
+module.exports = (env) => {
+  const plugins = env.development
+    ? apps.map(
+        (app) =>
+          new HtmlWebpackPlugin({
+            chunks: [app],
+            filename: path.join(__dirname, "dist", app, "index.html"),
+            template: path.join(__dirname, "src", "index.html"),
+          })
+      )
+    : [];
+
+  if (env.analyze) {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
+
+  return {
+    mode: env.production ? "production" : "development",
+    context: path.resolve(__dirname, "src", "apps"),
+    entry: apps.reduce((acc, app) => {
+      acc[app] = `./${app}/index.js`;
+      return acc;
+    }, {}),
+    output: {
+      filename: `[name].[contenthash].js`,
+      chunkFilename: `[name].chunk.[contenthash].js`,
+      path: path.join(__dirname, "dist"),
+      clean: true,
+    },
+    devServer: {
+      port: 3000,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ["", ".js", ".jsx"],
+    },
+    plugins,
+  };
 };
